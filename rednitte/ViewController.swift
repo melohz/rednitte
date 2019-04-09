7df1aed9e3bc8798f06c5cf9514d07bf4fab5b2c
Tom <melooh42@gmail.com>
2019-04-09 09:11:35 +0900

    Initial Commit

---
+++ rednitte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rednitte
+//
+//  Created by 板垣智也 on 2019/04/09.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rednitteTests/rednitteTests.swift
@@ -0,0 +1,34 @@
+//
+//  rednitteTests.swift
+//  rednitteTests
+//
+//  Created by 板垣智也 on 2019/04/09.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import XCTest
+@testable import rednitte
+
+class rednitteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
